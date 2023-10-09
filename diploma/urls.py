6d830a4ec3a25cb2c3e271c375bb1f770b38b6e0
Garpusha <alexander.sbitnev@yandex.ru>
2023-10-10 00:25:36 +0300

    small fixes

--- diploma/urls.py
+++ diploma/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from backend.views import UsersView
+from backend.views.user_views import UsersView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
